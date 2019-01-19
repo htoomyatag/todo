88f013749b0dc78815568ac6538a2d8117809ca5
htoomyataung <htoomyatag.webdev@gmail.com>
2019-01-19 18:58:11 +0630

    alert work

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 
 
 
-users = User.create([
+#users = User.create([
 
 
-{name: "htoomyatag1", email: "htoomyatag1@gmail.com", password: "12345678", password_confirmation: "12345678"},
-{name: "htoomyatag2", email: "htoomyatag2@gmail.com", password: "12345678", password_confirmation: "12345678"},
-{name: "theldarlykhin", email: "theldarlykhin.webdev@gmail.com", password: "12345678", password_confirmation: "12345678"},
+#{name: "htoomyatag1", email: "htoomyatag1@gmail.com", password: "12345678", password_confirmation: "12345678"},
+#{name: "htoomyatag2", email: "htoomyatag2@gmail.com", password: "12345678", password_confirmation: "12345678"},
+#{name: "theldarlykhin", email: "theldarlykhin.webdev@gmail.com", password: "12345678", password_confirmation: "12345678"},
 
 
-    ])
+ #   ])
